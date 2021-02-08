b15ec7b937e7c846720313f357f695b838ea7697
AmeerHamzaKhan97 <hamzakn97@gmail.com>
2021-02-08 19:47:22 +0530

    todo list in js

---
+++ todo js.js
@@ -0,0 +1,57 @@
+
+//localStorage.tasks=  JSON.stringify([]);
+
+updateList();
+
+function addEvent()  {
+    //console.log(localStorage.tasks);
+    let input = document.getElementById("Task_input");
+
+    let tasksArr = JSON.parse(localStorage.tasks);
+
+    tasksArr.push(input.value);
+
+    localStorage.tasks = JSON.stringify(tasksArr);
+
+    input.value = "";
+
+    updateList();
+}
+
+function updateList(){
+    let ul = document.getElementById("list-item");
+
+     let tasksArr = JSON.parse(localStorage.tasks);
+
+     let length  =tasksArr.length-1;
+     let text = "";
+
+     for(let i = length;i>=0;i--){
+          text +=
+            '<li class="list-group-item" id = "'+ i + '">' 
+            + tasksArr[i] +
+            '<span class="btn btn-danger btn-sm sub-item"  onclick ="removeItem(this)">Remove</span>  </li>';
+           
+        
+         
+     }
+     ul.innerHTML=text;
+}
+
+function removeItem(span_tag){
+    //console.log(obj);
+    let index_num= span_tag.parentElement.id;
+
+    let tasksArr = JSON.parse(localStorage.tasks);
+
+    tasksArr.splice(index_num,1);
+
+    localStorage.tasks = JSON.stringify(tasksArr);
+
+    updateList();
+
+
+
+
+}
+
